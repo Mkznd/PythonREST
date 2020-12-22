61862a327d65fabc281b218c398d8461fe4d4ec4
Mkznd <yzundai1@gmail.com>
2020-12-22 17:45:45 +0300

    Added HelloAPI and DefaultAPI views

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from typing import List
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class HelloAPIView(APIView):
+    """Test API view"""
+
+    def get(self, request, format_=None):
+        """Returns a list of APIView features"""
+        apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'is similar to a traditional django view',
+            'gives you the most controls over your application'
+        ]
+        return Response({'message': 'Hello!', 'apiview': apiview})
+
+class DefaultAPIView(APIView):
+    """APIView if nothing else is provided"""
+
+    def get(self, request, format_=None):
+        """Returns a basic cheering message"""
+        return Response({'Message': 'Great job, me!'})
